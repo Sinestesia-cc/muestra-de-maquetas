f0a8a40975fe3db3c9630cff1279aae14a55bc86
JulioJulioso <julio-ro@hotmail.cl>
2024-09-11 18:04:03 -0300

    Prueba maqueta

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2067533.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 195399987, 195399989, 195399997, 195399965, 195399967, 195399998, 195399999, 195400000, 195400002, 195399977, 195400003, 195399972, 195399974, 195399973 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
